062a50a672faa37dd5121c3eaf09988fdba3ea55
Hayatuddin <hayatuddinnur53@gmail.com>
2020-05-17 22:35:03 +0700

    TUGAS 1 JAVASCRIPT
    Tugas ini dikumpulkan untuk NIOMIC

---
+++ tugas1.js
@@ -0,0 +1,13 @@
+var nama_depan="Doni";
+var nama_depan2="Dina";
+var nama_depan3="Ulil";
+
+let nama_bel="Sundara";
+let nama_bel2="Sulistiawati";
+let nama_bel3="Albab";
+
+const umur= 20;
+const umur2=22;
+const umur3=23;
+
+console.log(nama_depan,nama_bel,umur,nama_depan2,nama_bel2,umur2, nama_depan3, nama_bel3, umur3 )
